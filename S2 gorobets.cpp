9825e4fa72158262ae015329f4342efc66b2b529
Nill-Kiggerz <159999157+Nill-Kiggerz@users.noreply.github.com>
2024-02-28 01:23:39 +0800

    Add files via upload

---
+++ S2 gorobets.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+  int a, b;
+  a = 13;
+  b = 4;
+  int c = a / b;
+  int d = a % b;
+  cout << c   << endl;
+  cout << d  << endl;
+  return 0;
+}
